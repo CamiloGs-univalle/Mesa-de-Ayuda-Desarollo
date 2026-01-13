21c08ad7c19df67f409f62235dfbee88b8177f2c
Camilo Gs Univalle <jhojan.garcia@correounivalle.edu.co>
2026-01-13 16:40:36 -0500

    CHA
    CHA

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> HELLO.
+          Edit <code>src/App.js</code> CHAO.
         </p>
         <a
           className="App-link"
